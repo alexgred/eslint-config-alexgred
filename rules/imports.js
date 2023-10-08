de79130c2fc24a8ed65ebdee338c25189cd53a88
Alex Dolzhenkov <alexgred.dev@gmail.com>
2023-10-08 14:29:55 +0300

    Rules for import

--- rules/imports.js
+++ rules/imports.js
@@ -37,5 +37,99 @@ module.exports = {
      * 🚫 Not fixable
      */
     'no-unused-modules': ['error', { unusedExports: true }],
+
+    /**
+     * Forbid import of modules using absolute paths.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-absolute-path.md
+     *
+     * 🔧 Fixable
+     */
+    'no-absolute-path': 'error',
+
+    /**
+     * Forbid a module from importing a module with a dependency path back to itself.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-cycle.md
+     *
+     * 🚫 Not fixable
+     */
+    'import/no-cycle': 'error',
+
+    /**
+     * Forbid importing packages through relative paths.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-relative-packages.md
+     *
+     * 🔧 Fixable
+     */
+    'import/no-relative-packages': 'warn',
+
+    /**
+     * Forbid a module from importing itself.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-self-import.md
+     *
+     * 🚫 Not fixable
+     */
+    'import/no-self-import': 'error',
+
+    /**
+     * Forbid unnecessary path segments in import and require statements.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-useless-path-segments.md
+     *
+     * 🔧 Fixable
+     */
+    'import/no-useless-path-segments': 'error',
+
+    /**
+     * Ensure all imports appear before other statements.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/first.md
+     *
+     * 🔧 Fixable
+     */
+    'import/first': 'error',
+
+    /**
+     * Enforce a newline after import statements.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/newline-after-import.md
+     *
+     * 🔧 Fixable
+     */
+    'import/newline-after-import': 'warn',
+
+    /**
+     * Forbid unassigned imports
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-unassigned-import.md
+     *
+     * 🚫 Not fixable
+     */
+    'import/no-unassigned-import': 'warn',
+
+    /**
+     * Enforce a convention in module import order.
+     *
+     * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
+     *
+     * 🔧 Fixable
+     */
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin', // Node.js built-in modules
+          'external', // Packages
+          'internal', // Aliased modules
+          'parent', // Relative parent
+          'sibling', // Relative sibling
+          'index', // Relative index
+        ],
+        'newlines-between': 'ignore',
+      },
+    ],
   },
 };
